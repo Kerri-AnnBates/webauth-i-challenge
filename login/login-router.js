c0f836e0da49a73ad0ff61e4759e4d99be47f662
Kerri-Ann <kaybee1jam@yahoo.com>
2019-12-17 18:56:54 -0500

    removed logout endpoint from login file.

--- login/login-router.js
+++ login/login-router.js
@@ -23,18 +23,4 @@ router.post('/', (req, res) => {
       })
 });
 
-router.get('/logout', (req, res) => {
-   if (req.session) {
-      req.session.destroy(error => {
-         if (error) {
-            res.status(500).json({ message: 'problem logging out' });
-         } else {
-            res.status(200).json({ message: 'You are logged out' });
-         }
-      });
-   } else {
-      res.status(200).end({ message: 'you are already logged out!' });
-   }
-})
-
 module.exports = router;
